6e1cf1b51ec68fc651e6f929e4f397f1fe7a4819
Vishal <62597759+Vishalk0001@users.noreply.github.com>
2020-09-03 18:23:57 +0930

    Add project files.

---
+++ Data/VishalMvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace VishalMvcMovie.Data
+{
+    public class VishalMvcMovieContext : DbContext
+    {
+        public VishalMvcMovieContext (DbContextOptions<VishalMvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
